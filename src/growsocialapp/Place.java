675f68e4cb08059d0ea120a431f37a258ba5d577
spaghettiSystems <41810162.m@acu.edu.eg>
2020-09-29 03:29:14 +0300

    Added missing items

--- src/growsocialapp/Place.java
+++ src/growsocialapp/Place.java
@@ -1,8 +1,59 @@
 package growsocialapp;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class Place extends GrowSocialApp {
+    @FXML StackPane mainContentPane;
+
+    public void locationPreview(MouseEvent event) throws Exception {
+        AnchorPane postPreview = FXMLLoader.load(getClass().getResource("LocationPreview.fxml"));
+
+        StackPane postPreviewHolder = new StackPane();
+        //postPreviewHolder.getChildren().add(postPreview);
+
+        //postPreviewHolder.layoutXProperty().bind(mainContentPane.widthProperty());
+        //postPreviewHolder.layoutYProperty().bind(mainContentPane.heightProperty());
+
+        Rectangle overlay = new Rectangle();
+        overlay.setOpacity(0.4);
+        overlay.setFill(Color.BLACK);
+        overlay.widthProperty().bind(postPreviewHolder.widthProperty());
+        overlay.heightProperty().bind(postPreviewHolder.heightProperty());
+
+
+        Button xButton = (Button)((HBox)((VBox)((StackPane)((HBox)postPreview.getChildren().get(0)).getChildren().get(1)).getChildren().get(0)).getChildren().get(0)).getChildren().get(0);
+        xButton.setOnAction(e -> {
+            mainContentPane.getChildren().remove(overlay);
+            mainContentPane.getChildren().remove(postPreviewHolder);
+        });
+
+        Button saveButton = (Button)((StackPane)((VBox)((StackPane)((HBox)postPreview.getChildren().get(0)).getChildren().get(1)).getChildren().get(0)).getChildren().get(5)).getChildren().get(0);
+        saveButton.setOnAction(e -> {
+            mainContentPane.getChildren().remove(overlay);
+            mainContentPane.getChildren().remove(postPreviewHolder);
+        });
+
+        Button cancelButton = (Button)((StackPane)((VBox)((StackPane)((HBox)postPreview.getChildren().get(0)).getChildren().get(1)).getChildren().get(0)).getChildren().get(4)).getChildren().get(0);
+        cancelButton.setOnAction(e -> {
+            mainContentPane.getChildren().remove(overlay);
+            mainContentPane.getChildren().remove(postPreviewHolder);
+        });
+
+        postPreviewHolder.getChildren().addAll(overlay, postPreview);
+
+
+        mainContentPane.getChildren().addAll(postPreviewHolder);
+    }
+
     public void Profilebtn(MouseEvent event) throws Exception {
         try {
 
